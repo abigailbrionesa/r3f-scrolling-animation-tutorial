90f655def84515ad03fa3dad546418c7d56d344a
Abigail Briones <abigailbrionesaranda@gmail.com>
2025-12-17 23:08:06 -0500

    experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,14 +1,15 @@
 import { OrbitControls, ScrollControls } from "@react-three/drei";
 import { Office } from "./Office";
 import { Overlay } from "./Overlay";
+import { Scroll } from "@react-three/drei";
 
 export const Experience = () => {
   return (
     <>
-      <ambientLight intensity={1} />
-      <OrbitControls enableZoom={false} />
       <ScrollControls pages={3} damping={0.25}>
-        <Overlay />
+           <Scroll html>
+        <div>hola</div>
+            </Scroll>
         <Office />
       </ScrollControls>
     </>
